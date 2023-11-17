28db17881fe745ee5aa19642915a920a4ce70d96
Saida Binta Alam <145463442+SaidaBinta87@users.noreply.github.com>
2023-11-17 13:25:48 +0600

    Update main.ts

--- smart_farming_v2/src/main.ts
+++ smart_farming_v2/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  app.use(
+     session({
+     secret: 'my-secret',
+     resave: false,
+     saveUninitialized: false,
+    cookie:{
+     maxAge: 300000
+    }
+     }),
+    );
+    
+  await app.listen(3000);
 }
 bootstrap();
