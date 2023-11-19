4a634ef79fabd5469437cbf123a3d7cfefa95c18
REZUAN ISLAM <113898856+RezuanIslam@users.noreply.github.com>
2023-11-20 01:45:01 +0600

    Update app.module.ts farmar added

--- smart_farming_v2/src/app.module.ts
+++ smart_farming_v2/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminModule } from './Admin/Admin.module';
+import { FarmerModule } from './farmer/farmer.module';
 import { ExpertModule } from './expert/expert.module';
 
 @Module({
